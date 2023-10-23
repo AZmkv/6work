81bab9ed493ec8faa6f1042b915ecb9d22a7e631
AZmkv <144830991+AZmkv@users.noreply.github.com>
2023-10-23 18:30:09 +0300

    Add files via upload

---
+++ Lab6_2/MathOperations.cs
@@ -0,0 +1,53 @@
+﻿namespace Lab6_2
+{
+    public class MathOperations
+    {
+        // Add methods
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Add(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+
+        // Subtract methods
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Subtract(params int[] numbers)
+        {
+            int result = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                result -= numbers[i];
+            }
+            return result;
+        }
+
+        // Multiply methods
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Multiply(params int[] numbers)
+        {
+            int result = 1;
+            foreach (int num in numbers)
+            {
+                result *= num;
+            }
+            return result;
+        }
+    }
+}
---
+++ Lab6_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using Lab6_2;
+
+class Program
+{
+    static void Main()
+    {
+        MathOperations math = new MathOperations();
+
+        // Addition
+        int sum1 = math.Add(5, 3);
+        Console.WriteLine($"Sum of two numbers: {sum1}");
+
+        int sum2 = math.Add(1, 2, 3, 4, 5);
+        Console.WriteLine($"Sum of an array of numbers: {sum2}");
+
+        // Subtraction
+        int difference1 = math.Subtract(10, 3);
+        Console.WriteLine($"Difference of two numbers: {difference1}");
+
+        int difference2 = math.Subtract(10, 2, 3, 1);
+        Console.WriteLine($"Difference of an array of numbers: {difference2}");
+
+        // Multiplication
+        int product1 = math.Multiply(2, 3);
+        Console.WriteLine($"Product of two numbers: {product1}");
+
+        int product2 = math.Multiply(2, 3, 4);
+        Console.WriteLine($"Product of an array of numbers: {product2}");
+    }
+}
